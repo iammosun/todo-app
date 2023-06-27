d6c2b59419567d496a4f04c54a58497537d4d42d
Mosunmola Owolade <mbowolade@gmail.com>
2023-06-27 21:09:23 +0100

    testing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './Components/MainPage';
+
+function App() {
+  return (
+    <div className="mainBody">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
